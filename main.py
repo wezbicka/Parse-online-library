7c626682f84299610540684662463dd8194dcfa4
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-21 14:16:10 +0300

    Download book

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import requests
 
 
-url = "https://dvmn.org/filer/canonical/1542890876/16/"
+if __name__ == "__main__":
+    url = "https://tululu.org/txt.php?id=32168"
+    response = requests.get(url)
+    response.raise_for_status() 
 
-response = requests.get(url)
-response.raise_for_status() 
-
-filename = 'dvmn.svg'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+    filename = 'book.txt'
+    with open(filename, 'wb') as file:
+        file.write(response.content)
