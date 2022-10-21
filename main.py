9ff2d88348c089485e120743a29b8b11cc4e2f0a
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-21 14:24:59 +0300

    Download books from tululu.org

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import os
+
 import requests
 
 
 if __name__ == "__main__":
-    url = "https://tululu.org/txt.php?id=32168"
-    response = requests.get(url)
-    response.raise_for_status() 
+    directory = "books"
+    os.makedirs(directory, exist_ok=True)
+    for id in range(10):
+        url = f"https://tululu.org/txt.php?id={id + 1}"
+        response = requests.get(url)
+        response.raise_for_status() 
 
-    filename = 'book.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+        filename = f'book{id}.txt'
+        with open(os.path.join(directory, filename), 'wb') as file:
+            file.write(response.content)
