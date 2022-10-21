c4986481b94fbdd0c6e58231b66e819b8ae4e494
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-21 15:47:01 +0300

    Create bs4_tutorial.py
    I created this file for learning parsing

---
+++ bs4_tutorial.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://www.franksonnenbergonline.com/blog/are-you-grateful/'
+response = requests.get(url)
+response.raise_for_status()
+soup = BeautifulSoup(response.text, 'lxml')
+# print(soup.prettify())
+title_tag = soup.find('main').find('header').find('h1')
+title_text = title_tag.text
+print(title_text)
+image_url = soup.find('img', class_='attachment-post-image')['src']
+print(image_url)
