cd08505fb925bed093ec7aff9e4026f5f40824c9
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-21 15:18:18 +0300

    Add check_for_redirect

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import os
 import requests
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
 if __name__ == "__main__":
     directory = "books"
     os.makedirs(directory, exist_ok=True)
-    for id in range(10):
-        url = f"https://tululu.org/txt.php?id={id + 1}"
+    for id in range(1, 11):
+        url = f"https://tululu.org/txt.php?id={id}"
         response = requests.get(url)
-        response.raise_for_status() 
+        response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except requests.exceptions.HTTPError:
+            continue
 
-        filename = f'book{id}.txt'
+        filename = f'id{id}.txt'
         with open(os.path.join(directory, filename), 'wb') as file:
             file.write(response.content)
