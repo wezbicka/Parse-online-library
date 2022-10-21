16b419b961ece1be95f2c62256a07e4db57eb570
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-21 13:32:44 +0300

    Download image devman.org

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import os
-
 import requests
 
 
-# url = "https://dvmn.org/filer/canonical/1542890876/16/"
+url = "https://dvmn.org/filer/canonical/1542890876/16/"
 
-# response = requests.get(url)
-# response.raise_for_status() 
+response = requests.get(url)
+response.raise_for_status() 
 
-# filename = 'dvmn.svg'
-# with open(filename, 'wb') as file:
-#     file.write(response.content)
-if __name__ == "__main__":
-    directory = "books"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    url = "https://tululu.org/txt.php?id=32168"
-    response = requests.get(url)
-    response.raise_for_status() 
-
-    filename = 'book.txt'
-    with open(filename, 'wb') as file:
-        file.write(response.content)
+filename = 'dvmn.svg'
+with open(filename, 'wb') as file:
+    file.write(response.content)
