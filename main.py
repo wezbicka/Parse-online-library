ad68d3e1af8a4a2e9bffda9430323b501e40c040
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-22 16:44:31 +0300

    Parse books genres

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import urljoin, urlsplit, unquote, urlparse
+from urllib.parse import urljoin, urlsplit, unquote
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,6 +16,7 @@ def download_txt(url, filename, folder='books/'):
     filepath = os.path.join(folder, sanitize_filename(f"{filename}.txt"))
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
     with open(filepath, 'wb') as file:
         file.write(response.content)
     return filepath
@@ -26,6 +27,7 @@ def download_image(url, filename, folder='images/'):
     filepath = os.path.join(folder, sanitize_filename(f"{filename}"))
     response = requests.get(url)
     response.raise_for_status()
+    check_for_redirect(response)
     with open(filepath, 'wb') as file:
         file.write(response.content)
     return filepath
@@ -42,35 +44,42 @@ def parse(response):
     for comment_tag in comments_tag:
         comment = comment_tag.find('span', class_='black').text
         comments.append(comment)
+    genre_tags = soup.find('span', class_='d_book').find_all("a")
+    genres = []
+    for tag in genre_tags: 
+        genres.append(tag.text)
     return {
         "title": book_title.strip(),
         "author": author_book.strip(),
         "image": image_url,
         "comments": comments,
+        "genres": genres,
     }
 
 
 if __name__ == "__main__":
     for id in range(1, 11):
         try:
+            url = f"https://tululu.org/b{id}/"
+            response = requests.get(url)
+            response.raise_for_status()
+            check_for_redirect(response)
+            book = parse(response)
+            print("Заголовок:", book['title'])
+            url_image = book['image']
+            print(url_image)
+
             download_url = f'https://tululu.org/txt.php?id={id}'
             response = requests.get(download_url)
             response.raise_for_status()
-            check_for_redirect(response)
+            filename = f'{id}. {book["title"]}'
+            download_txt(download_url, filename)
+
+            filename = unquote(urlsplit(url_image).path).split("/")[-1]
+            print(filename)
+            download_image(url_image, filename)
+
+            # print(book['comments'])
         except requests.exceptions.HTTPError:
             continue
-
-        url = f"https://tululu.org/b{id}/"
-        response = requests.get(url)
-        response.raise_for_status()
-        book = parse(response)
-        print("Заголовок:", book['title'])
-        url_image = book['image']
-        print(url_image)
-        filename = f'{id}. {book["title"]}'
-        download_txt(download_url, filename)
-        filename = unquote(urlsplit(url_image).path).split("/")[-1]
-        print(filename)
-        download_image(url_image, filename)
-
-        print(book['comments'])
+        
