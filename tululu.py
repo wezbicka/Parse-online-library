0b303f0ad599f4eb5d7f7a607fa182c845559bd2
Polina <75391420+wezbicka@users.noreply.github.com>
2022-10-27 21:48:03 +0300

    Separate code for function

--- tululu.py
+++ tululu.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import sys
 import logging
+from time import sleep
 
 from urllib.parse import urljoin, urlsplit, unquote
 import requests
@@ -80,33 +81,69 @@ def parse_book_page(html, base_url):
     }
 
 
-if __name__ == "__main__":
-    logging.basicConfig(filename='error.log', filemode='w')
-    start_id, end_id = fetch_book_id()
-    for book_id in range(start_id, end_id + 1):
+def handle_errors(func_, *args):
+    first_reconnection = True
+    while True:
         try:
-            url = f"https://tululu.org/b{book_id}/"
-            response = requests.get(url)
-            response.raise_for_status()
-            check_for_redirect(response)
-            book = parse_book_page(response.text, response.url)
-            book_title = book['title']
-            url_image = book['image']
-            print("Заголовок:", book_title)
-            print(url_image)
-            filename = f'{book_id}. {book_title}'
-            print(book['comments'])
-            payload = {"id": book_id}
-            download_url = f'https://tululu.org/txt.php'
-            download_txt(download_url, payload, filename)
-            filename = unquote(urlsplit(url_image).path).split("/")[-1]
-            print(filename)
-            download_image(url_image, filename)
+            response = func_(*args)
+
+            if not first_reconnection:
+                print('Connection is restored.')
+
+            return response
+
         except requests.exceptions.HTTPError:
             logging.warning(f"Redirect. The book {book_id} not found")
             print(
                 f'Книги №{book_id} не найдена!',
                 file=sys.stderr
             )
-            continue
-        
+            sys.exit("Error!")
+        except requests.ConnectionError as connect_err:
+            if first_reconnection:
+                print('Connection is down!')
+                print(logging.warning(connect_err), file=sys.stderr)
+                print('Retry in 5 seconds')
+                sleep(5)
+                first_reconnection = False
+            else:
+                print(
+                    logging.warning('Connection is still down!'),
+                    file=sys.stderr
+                    )
+                print('Retry in 15 seconds')
+                sleep(15)
+
+
+def download_books_and_images(book_indexes):
+    parsed_books = []
+    for book_id in book_indexes:
+        parsed_books.append(
+            handle_errors(
+                parse_book_page,
+                book_id
+            )
+        )
+        url = f"https://tululu.org/b{book_id}/"
+        response = requests.get(url)
+        response.raise_for_status()
+        check_for_redirect(response)
+        book = parse_book_page(response.text, response.url)
+        book_title = book['title']
+        url_image = book['image']
+        print("Заголовок:", book_title)
+        print(url_image)
+        filename = f'{book_id}. {book_title}'
+        print(book['comments'])
+        payload = {"id": book_id}
+        download_url = f'https://tululu.org/txt.php'
+        download_txt(download_url, payload, filename)
+        filename = unquote(urlsplit(url_image).path).split("/")[-1]
+        print(filename)
+        download_image(url_image, filename)
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='error.log', filemode='w')
+    start_id, end_id = fetch_book_id()
+    book_indexes = range(start_id, end_id + 1)
+    download_books_and_images(book_indexes)
