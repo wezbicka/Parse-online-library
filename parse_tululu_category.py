6734ea28f2d66cb7ce5be32e90e1ada829fdedf1
Polina <75391420+wezbicka@users.noreply.github.com>
2023-08-10 23:05:28 +0300

    Get all book links from 10 pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 
-url = 'https://tululu.org/l55/'
-response = requests.get(url)
-response.raise_for_status()
-soup = BeautifulSoup(response.text, 'lxml')
-books = soup.find_all("div", class_="bookimage")
+for page_number in range(1, 11):
+    url = f'https://tululu.org/l55/{page_number}'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    books = soup.find_all("div", class_="bookimage")
 
-for book in books:
-    book_id = book.find("a")["href"]
-    book_url = urljoin(url, book_id)
-    print(book_url)
+    for book in books:
+        book_id = book.find("a")["href"]
+        book_url = urljoin(url, book_id)
+        print(book_url)
